884f8414f3e573c093a4d713f326291ff4a90f54
Jarrett0203 <jarrett0203@gmail.com>
2022-12-20 22:31:13 +0800

    finished v1 of weather app with working search

--- src/index.js
+++ src/index.js
@@ -1,19 +1,44 @@
+import {format} from 'date-fns'
+
+const API_key = "5a5953b54684ec61924a024558f995b6";
+
+const description = document.querySelector(".description");
+const city = document.querySelector(".city");
+const time = document.querySelector(".time");
 const temp = document.querySelector(".temp");
+const overall = document.querySelector(".overall");
+const city_input = document.querySelector(".search");
+const error = document.querySelector(".error-msg");
+
+const feels_like = document.querySelector(".feels_like_info");
+const humidity = document.querySelector(".humidity_info");
+const pressure = document.querySelector(".pressure_info");
+const wind = document.querySelector(".wind_info");
+
 async function getWeatherData(location = 'London') {
+  console.log(location);
   const response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${location},uk&APPID=5a5953b54684ec61924a024558f995b6`,
+    `http://api.openweathermap.org/data/2.5/weather?q=${location.trim()}&APPID=${API_key}`,
     { mode: "cors" }
   );
-  const weatherData = await response.json();
-  console.log(weatherData);
-  
-  const processedData = processWeatherData(weatherData);
-  displayWeatherData(processedData);
+  if (response.status == "404") {
+    error.classList.remove("hidden");
+  }
+  else {
+    error.classList.add("hidden");
+    const weatherData = await response.json();
+    console.log(weatherData);
+    const processedData = processWeatherData(weatherData);
+    console.log(processedData);
+    displayWeatherData(processedData);
+  }
 }
 
 function processWeatherData(weatherData) {
   const processedData = {
-    name: weatherData.name,
+    clouds: weatherData.clouds.all,
+    coord: weatherData.coord,
+    city: weatherData.name,
     main: weatherData.main,
     sys: weatherData.sys,
     weather: weatherData.weather[0],
@@ -23,17 +48,48 @@ function processWeatherData(weatherData) {
 }
 
 function displayWeatherData(processedData) {
-  const celsius = convertToC(processedData.main.temp);
+  const description_string = processedData.weather.description;
+  const temp_value = processedData.main.temp;
+  const weather_icon = processedData.weather.icon;
+
+  const feels_like_temp = processedData.main.feels_like;
+  const humidity_value = processedData.main.humidity;
+  const pressure_value = processedData.main.pressure;
+  const wind_speed_value = processedData.wind.speed;
+  const currentDate = new Date();
+
+  description.textContent = capitalizeFirstLetter(description_string);
+  city.textContent = processedData.city;
+  time.textContent = "Accurate as of " + format(currentDate, 'dd-MM-yy HH:mm:ss');
+  overall.src = `https://openweathermap.org/img/wn/${weather_icon}.png`;  
+
+  humidity.textContent = humidity_value + " %";
+  pressure.textContent = pressure_value + " hPa";
+  wind.textContent = wind_speed_value + " m/s";
+  
+  convertToC(temp, temp_value);
+  convertToC(feels_like, feels_like_temp);
+}
+
+function capitalizeFirstLetter(string) {
+  return string.split(" ").map(word => word[0].toUpperCase() + word.substring(1)).join(" ");
+}
+
+function convertToF(temp, temp_value) {
+  const fahrenheit = temp_value * 9/5 - 459.67;
+  temp.textContent = Math.round(fahrenheit * 10) / 10 + " °C";
+}
+
+function convertToC(temp, temp_value) {
+  const celsius = temp_value - 273.15;
   temp.textContent = Math.round(celsius * 10) / 10 + " °C";
 }
 
-function convertToF(temp) {
-  return temp * 9/5 - 459.67;
-}
-
-function convertToC(temp) {
-  return temp - 273.15;
-}
-
+city_input.addEventListener("keypress", e => 
+  {if (e.key === 'Enter') {
+    getWeatherData(e.target.value);
+    e.target.value = '';
+  }}
+);
 
 getWeatherData();
